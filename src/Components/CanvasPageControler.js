61ad44b7a8fd16ed74629f5f7cf8140814d4d686
Wojciech Brożek <wobrozek@gmail.com>
2023-01-10 16:31:20 +0100

    add simple docx download

--- src/Components/CanvasPageControler.js
+++ src/Components/CanvasPageControler.js
@@ -5,8 +5,48 @@ import AddIcon from '@mui/icons-material/Add';
 import DownloadIcon from '@mui/icons-material/Download';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { pageContext } from '../Pages/AlbumPage';
+import { Document, Packer, Paragraph, ImageRun } from 'docx';
+import { saveAs } from 'file-saver';
 
 export const CanvasPageControler = () => {
+	const saveAlbum = () => {
+		const image = createDocxImage(300, 300, 300, 300);
+		const doc = new Document({
+			sections: [
+				{
+					children: [
+						new Paragraph({
+							children: [ image ]
+						})
+					]
+				}
+			]
+		});
+
+		Packer.toBlob(doc).then((blob) => {
+			saveAs(blob, 'Album Ślubny.docx');
+		});
+	};
+
+	const createDocxImage = (heightPhoto, widthPhoto, offsetLeft, offsetTop) => {
+		let image = new ImageRun({
+			data: context.allPages[0][0].base64,
+			transformation: {
+				width: widthPhoto,
+				height: heightPhoto
+			},
+			floating: {
+				horizontalPosition: {
+					offset: offsetLeft
+				},
+				verticalPosition: {
+					offset: offsetTop
+				}
+			}
+		});
+		return image;
+	};
+
 	const context = useContext(pageContext);
 	return (
 		<nav className="flex-space-between navbar">
@@ -27,7 +67,7 @@ export const CanvasPageControler = () => {
 					<DeleteIcon />
 				</button>
 				<button>
-					<DownloadIcon />
+					<DownloadIcon onClick={saveAlbum} />
 				</button>
 			</div>
 		</nav>

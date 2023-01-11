2dc41c698f786d66d75302b3eedaa676152c34bd
Wojciech Brożek <wobrozek@gmail.com>
2023-01-11 11:56:12 +0100

    add positioning

--- src/Components/PhotoCanvas.js
+++ src/Components/PhotoCanvas.js
@@ -35,17 +35,19 @@ function PhotoCanvas(props) {
 	);
 
 	const photoSizeAndPosition = () => {
-		console.log(context.allPages);
 		const canvasHeight = context.cordinantsRef.current.height;
 		const canvasWidth = context.cordinantsRef.current.width;
+		let lastI = null;
 
 		context.allPages[0].map((_, columnIndex) => {
 			let rowIndex = 0;
 			context.allPages.map((row) => {
 				if (!row[columnIndex]) return;
 				let i = new Image();
+				lastI = i;
 
 				i.onload = function() {
+					console.log(i.height, i.width);
 					context.allPages[i.rowIndex][i.columnIndex].height = i.height;
 					context.allPages[i.rowIndex][i.columnIndex].width = i.width;
 				};
@@ -57,65 +59,67 @@ function PhotoCanvas(props) {
 			});
 		});
 
-		context.allPages[0].map((_, columnIndex) => {
-			let rowIndex = 0;
-			let top = 5;
-			let left = 5;
-			context.allPages.map((row) => {
-				if (!row[columnIndex]) return;
+		lastI.onload = function() {
+			context.allPages[0].map((_, columnIndex) => {
+				let rowIndex = 0;
+				context.allPages.map((row) => {
+					if (!row[columnIndex]) return;
 
-				const [ newLeft, newTop ] = setPosition(top, left, columnIndex, rowIndex, canvasWidth, canvasHeight);
+					const [ newLeft, newTop ] = setPosition(columnIndex, rowIndex, canvasWidth, canvasHeight);
 
-				left = newLeft;
-				top = newTop;
-				rowIndex += 1;
+					rowIndex += 1;
+				});
 			});
-		});
-
-		console.log(context.allPages);
+		};
 	};
 
-	function setPosition(top, left, columnIndex, rowIndex, canvasWidth, canvasHeight) {
-		console.log(context.allPages[rowIndex][columnIndex]);
+	function setPosition(columnIndex, rowIndex, canvasWidth, canvasHeight) {
 		let photoWidth = context.allPages[rowIndex][columnIndex].width;
 		let photoHeight = context.allPages[rowIndex][columnIndex].height;
 		let halfCanvasWidth = canvasWidth / 2 - 10;
 		let halfCanvasHeight = canvasHeight / 2 - 10;
 		let aspectRatio = photoWidth / photoHeight;
+		let left = 0;
+		let top = 0;
 
-		if (top > left) {
-			if (photoHeight > photoWidth) {
-				setHorizontal(top, left, columnIndex, rowIndex, aspectRatio, halfCanvasHeight);
-				left = halfCanvasWidth;
-			} else {
-				setVertical(top, left, columnIndex, rowIndex, aspectRatio, halfCanvasWidth);
-				left = halfCanvasWidth;
-			}
-		} else if (top < left) {
-			if (photoHeight > photoWidth) {
-				setHorizontal(top, left, columnIndex, rowIndex, aspectRatio, halfCanvasHeight);
-				top = halfCanvasHeight;
-			} else {
-				setVertical(top, left, columnIndex, rowIndex, aspectRatio, halfCanvasWidth);
-				top = halfCanvasHeight;
-			}
-		} else if (top === left) {
-			if (photoHeight > photoWidth) {
-				setHorizontal(top, left, columnIndex, rowIndex, aspectRatio, halfCanvasHeight);
-				left = halfCanvasWidth;
-			} else {
-				setVertical(top, left, columnIndex, rowIndex, aspectRatio, halfCanvasWidth);
-				left = halfCanvasWidth;
-			}
+		switch (columnIndex) {
+			case 0:
+				left = 10;
+				top = 10;
+				break;
+			case 1:
+				left = 10 + halfCanvasWidth;
+				top = 10;
+				break;
+			case 2:
+				top = 10 + halfCanvasHeight;
+				left = 10;
+				break;
+			case 3:
+				top = 10 + halfCanvasHeight;
+				left = 10 + halfCanvasWidth;
+				break;
+			default:
+				top = 0;
+				left = 0;
 		}
-		return [ top, left ];
+
+		if (photoHeight > photoWidth) {
+			setHorizontal(top, left, columnIndex, rowIndex, aspectRatio, halfCanvasHeight);
+			left = halfCanvasWidth;
+		} else {
+			setVertical(top, left, columnIndex, rowIndex, aspectRatio, halfCanvasWidth);
+			left = halfCanvasWidth;
+		}
+
+		return [ left, top ];
 	}
 
 	function setVertical(top, left, columnIndex, rowIndex, aspectRatio, halfCanvasWidth) {
 		context.allPages[rowIndex][columnIndex].top = top;
 		context.allPages[rowIndex][columnIndex].left = left;
 		context.allPages[rowIndex][columnIndex].width = halfCanvasWidth;
-		context.allPages[rowIndex][columnIndex].height = halfCanvasWidth * aspectRatio;
+		context.allPages[rowIndex][columnIndex].height = halfCanvasWidth / aspectRatio;
 	}
 
 	function setHorizontal(top, left, columnIndex, rowIndex, aspectRatio, halfCanvasHeight) {
@@ -202,11 +206,12 @@ function PhotoCanvas(props) {
 		context.page.map((element) => {
 			fabric.Image.fromURL(`data:image/jpg;base64,${element.base64}`, function(oImg) {
 				oImg.set({
-					left: 10,
-					top: 10
+					left: element.left,
+					top: element.top
 				});
 				oImg.id = element.id;
-				oImg.scaleToWidth(200);
+				oImg.scaleToWidth(element.width);
+				oImg.scaleToHeight(element.height);
 				canvi.add(oImg);
 			});
 		});
